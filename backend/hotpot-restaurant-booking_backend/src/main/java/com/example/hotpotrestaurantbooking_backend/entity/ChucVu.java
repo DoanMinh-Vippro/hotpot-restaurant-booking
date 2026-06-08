2e58cef26219306d486fc3ed794438898bbe2470
Nguyễn Văn Đạt <nvandat773@gmail.com>
2026-06-08 22:23:11 +0700

    Dat done

--- backend/hotpot-restaurant-booking_backend/src/main/java/com/example/hotpotrestaurantbooking_backend/entity/ChucVu.java
+++ backend/hotpot-restaurant-booking_backend/src/main/java/com/example/hotpotrestaurantbooking_backend/entity/ChucVu.java
@@ -1,19 +1,24 @@
 package com.example.hotpotrestaurantbooking_backend.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
 @Entity
-@Builder
 @Table(name = "ChucVu")
+@Builder
 public class ChucVu {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer idChucVu;
-    private String maChucVu;
-    private String tenChucVu;
+    @Column(name = "id_chuc_vu")
+    private Integer id;
+    @Column(name = "ma_chuc_vu")
+    private String ma_chuc_vu;
+    @Column(name = "ten_chuc_vu")
+    private String ten_chuc_vu;
 }
--- frontend/hotpot-restaurant-booking_frontend/hotpot-restaurant-booking/src/router/index.ts
+++ frontend/hotpot-restaurant-booking_frontend/hotpot-restaurant-booking/src/router/index.ts
@@ -80,7 +80,27 @@ const router = createRouter({
       path: '/dat-ban',
       name: 'dat-ban',
       component: () => import('@/views/DatBanView.vue'),
-    }
+    },
+     {
+      path: '/nhan-vien',
+      name: 'nhan-vien',
+    component: ()=> import('@/views/NhanVienView.vue'),
+    },
+    {
+      path: '/tai-khoan',
+      name: 'tai-khoan',
+    component: ()=> import('@/views/TaiKhoanView.vue'),
+    },
+     {
+      path: '/chuc-vu',
+      name: 'chuc-vu',
+    component: ()=> import('@/views/ChucVuView.vue'),
+    },
+    {
+      path: '/thong-ke',
+      name: 'thong-ke',
+    component: ()=> import('@/views/ThongKeView.vue'),
+    },
   ],
 })
 
